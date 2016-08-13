5f03df2f66dc1344d523ed92f53a526d3d0fc025
byronBarillas <Byronbarillas@live.com.au>
2016-08-13 23:59:45 +1000

    Updated to include comment on largest to smallest

--- 02.1_INSERTION SORT.py
+++ 02.1_INSERTION SORT.py
@@ -24,7 +24,7 @@ for j in xrange(1,len(A)):
     key = A[j]
     #Insert A[j] into the sorted sequence A[1... j-1]
     i = j - 1
-    while (i>-1) and A[i] > key:
+    while (i>-1) and A[i] > key:  #Flip the second inequality and you will get a largest to smallest sequence
         A[i+1] = A[i]
         i = i -1
     A[i+1] = key
